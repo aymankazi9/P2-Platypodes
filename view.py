a151da3b23efdd42c045c59eaab9cf0d4a5f4c59
Pragadeesh Raj <pragadeesh3000@gmail.com>
2021-01-27 19:48:49 -0800

    Added alt dimension to website.

--- view.py
+++ view.py
@@ -4,10 +4,12 @@ from flask import Flask
 import model
 from flask import render_template
 import sqlite3 as sl3
+
 app = Flask(__name__)
 import stats
 import storefb
 from flask import request
+
 
 @app.route('/')  # app routes to various html pages that we have assigned it to
 def home_route():
@@ -69,19 +71,21 @@ def fl_route():
 def ny_route():
     return render_template("ny.html", model=model.setup())
 
+
 @app.route('/map/')
 def map_route():
     conn = sl3.connect('platypodes.db')
-    c=conn.cursor()
+    c = conn.cursor()
     c.execute("SELECT * FROM COVIDSTATS")
-    #rows=c.fetchall()
-    #print(rows)
-    #with conn:
-        #data = conn.execute("SELECT * FROM COVIDSTATS")
-    #for row in data:
-        #print(row)
+    # rows=c.fetchall()
+    # print(rows)
+    # with conn:
+    # data = conn.execute("SELECT * FROM COVIDSTATS")
+    # for row in data:
+    # print(row)
 
     return render_template("map.html", rows=c.fetchall(), model=model.setup())
+
 
 @app.route('/feedback_form', methods=['POST'])
 def feedback_form():
@@ -90,7 +94,7 @@ def feedback_form():
     mailid = request.form['email']
     service = request.form['type']
     opinion = request.form['feedback']
-    storefb.insertfeedback(fname,lname,mailid,service,opinion)
+    storefb.insertfeedback(fname, lname, mailid, service, opinion)
     '''
     print (fname)
     print (lname)
@@ -100,10 +104,12 @@ def feedback_form():
     '''
     return render_template("feedback.html", model=model.setup())
 
+
 @app.route('/tos&p/')
 def tosp_route():
     return render_template("tos&p.html", model=model.setup())
 
-@app.route('/deliverable/')
-def deliverable_route():
-    return render_template("deliverable.html", model=model.setup())
+
+@app.route('/home2/')
+def home2_route():
+    return render_template("home2.html", model=model.setup())
